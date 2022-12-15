d347ea1d75b9bf9bc00836eda31b1571b34d0baf
Sergio Andres Rubiano Rodriguez <srubiano@emeal.nttdata.com>
2022-12-15 11:43:24 -0500

    Add Files

--- Beam_BigQuery/main_gcs_to_bigquery.py
+++ Beam_BigQuery/main_gcs_to_bigquery.py
@@ -1,50 +1,61 @@
+# Import necessary modules from Apache Beam
 import os
-import argparse
 import apache_beam as beam
-from apache_beam import PCollection
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToBigQuery
 from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.pipeline import Pipeline
+from apache_beam.pvalue import PCollection
+from apache_beam.transforms import PTransform
 
 #Service
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "./service-account.json"
 
 table = "dt-data-analytics:test.sample"
-table_schema = "columna1:STRING,columna2:STRING,columna3:STRING,columna4:STRING,columna5:STRING,columna6:STRING,columna7:STRING,columna8:STRING,columna9:STRING,columna10:STRING,columna11:STRING,columna12:STRING"
-
-def main():
-    parser = argparse.ArgumentParser(description="Read Data from Cloud Storage")
-    parser.version="1.0"
-    parser.add_argument("--input", help="Input query")
-    parser.add_argument("--out_data", help= "out data")
-
-    our_args, beam_args = parser.parse_known_args()
-    run_pipeline(our_args, beam_args)
+table_schema = "columna1:INTEGER, columna2:STRING, columna3:STRING"
 
 
-def run_pipeline(custom_args, beam_args):
-    input = custom_args.input
-    out_data = custom_args.out_data
+# Define a transform to parse the CSV data and convert it to a format that can be written to BigQuery
+class ParseData(PTransform):
+  def expand(self, pcollection):
+    return (pcollection
+            | 'Split Lines' >> beam.Map(lambda x: x.split(','))
+            | 'Create Dict' >> beam.Map(lambda x: {'columna1': x[0], 'columna2': x[1], 'columna3': x[2]})
+           )
+
+# Create a pipeline object
+pipeline = Pipeline()
+
+# Specify the source of the data in Cloud Storage
+data_source = pipeline | 'Read Data Cloud Storage' >> ReadFromText('gs://pruebas-dt-data-analytics/data/sample.csv', skip_header_lines = 1)
+
+# Apply the transform to the source data
+parsed_data = data_source | 'Parse Data' >> ParseData()
+
+# Define a transform to write the parsed data to BigQuery
+class WriteToBQ(PTransform):
+  def expand(self, pcollection):
+    return (pcollection
+            | 'Write to BigQuery' >> WriteToBigQuery(table,
+                                                    schema=table_schema,
+                                                    write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE, #delete existing rows.
+                                                    create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED, #create if does not exist. 
+                                                    custom_gcs_temp_location="gs://pruebas-dt-data-analytics/tmp/")
+           )
+
+# Apply the transform to write the data to BigQuery
+_ = parsed_data | 'Write to BigQuery' >> WriteToBQ()
 
 
-    options = {"project": "dt-data-analytics",
-              "runner": 'Direct',  # <------ Runtine
-              "temp_location": "gs://pruebas-dt-data-analytics/tmp/",
-              "region": "us-east1" }
+options = {"project": "dt-data-analytics",
+            "runner": 'Direct',  # <------ Runtine
+            'staging_location':"gs://pruebas-dt-data-analytics/tmp/",
+            "temp_location": "gs://pruebas-dt-data-analytics/tmp/",
+            "region": "us-east1"}
 
-    pipeline_options = beam.pipeline.PipelineOptions(beam_args,**options)
+# Create a PipelineOptions object from the dictionary
+pipeline_options = PipelineOptions.from_dictionary(options)
 
-    #Creating pipeline
-    with beam.Pipeline(options=pipeline_options) as pipeline:
-
-        #Read data
-        query_results = pipeline | "Input query" >> beam.io.ReadFromText("gs://pruebas-dt-data-analytics/data/sample.csv",skip_header_lines=1)
-        #query_results | beam.Map(print)
-        query_results | "Write to BigQuery" >> beam.io.WriteToBigQuery(
-                                                                      table=table,
-                                                                      schema=table_schema,
-                                                                      write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
-                                                                      create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
-
-
-
-if __name__ == "__main__":
-    main()
+# Execute the pipeline with the specified options
+result = pipeline.run(pipeline_options)
+result.wait_until_finish()
