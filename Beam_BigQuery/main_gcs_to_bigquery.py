69b1b762c99a823c9fa26c9aad2270e92beefaae
Sergio Andres Rubiano Rodriguez <srubiano@emeal.nttdata.com>
2022-12-23 08:26:44 -0500

    Add file

--- Beam_BigQuery/main_gcs_to_bigquery.py
+++ Beam_BigQuery/main_gcs_to_bigquery.py
@@ -51,7 +51,8 @@ def run_pipeline(our_args, beam_args):
                 'staging_location':"gs://pruebas-dt-data-analytics/tmp/",
                 "temp_location": "gs://pruebas-dt-data-analytics/tmp/",
                 "region": "us-east1",
-                "experiments": ['shuffle_mode=service']} # <--- reduce resource usage,  only for batch jobs
+                "experiments": ['shuffle_mode=service'], # <--- reduce resource usage,  only for batch jobs
+                } 
 
     pipeline_options = beam.pipeline.PipelineOptions(beam_args,**options)
     
